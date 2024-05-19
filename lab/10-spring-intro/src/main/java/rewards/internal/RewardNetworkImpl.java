7b8012761cb7cec8427d92d2cd7380484d7d65d7
Kevin Michel <kevin.rabefaritra@gmail.com>
2024-05-19 12:30:27 +0200

    10-spring-intro

--- lab/10-spring-intro/src/main/java/rewards/internal/RewardNetworkImpl.java
+++ lab/10-spring-intro/src/main/java/rewards/internal/RewardNetworkImpl.java
@@ -1,9 +1,13 @@
 package rewards.internal;
 
+import common.money.MonetaryAmount;
+import rewards.AccountContribution;
 import rewards.Dining;
 import rewards.RewardConfirmation;
 import rewards.RewardNetwork;
+import rewards.internal.account.Account;
 import rewards.internal.account.AccountRepository;
+import rewards.internal.restaurant.Restaurant;
 import rewards.internal.restaurant.RestaurantRepository;
 import rewards.internal.reward.RewardRepository;
 
@@ -53,6 +57,20 @@ public class RewardNetworkImpl implements RewardNetwork {
 		// TODO-07: Write code here for rewarding an account according to
 		//          the sequence diagram in the lab document
 		// TODO-08: Return the corresponding reward confirmation
-		return null;
+		// 1. Get the associated account
+		String creditCardNumber = dining.getCreditCardNumber();
+		Account account = accountRepository.findByCreditCard(creditCardNumber);
+
+		String merchantNumber = dining.getMerchantNumber();
+		Restaurant restaurant = restaurantRepository.findByMerchantNumber(merchantNumber);
+
+		// 2. Calculate benefits
+		MonetaryAmount monetaryAmount = restaurant.calculateBenefitFor(account, dining);
+		AccountContribution accountContribution = account.makeContribution(monetaryAmount);
+
+		accountRepository.updateBeneficiaries(account);
+
+		// 3. Contribute
+		return rewardRepository.confirmReward(accountContribution, dining);
 	}
 }

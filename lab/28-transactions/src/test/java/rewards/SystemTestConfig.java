3f1ea4c67b6ed9d2c4244bf98547cd5ed47a6c66
Kevin Michel <kevin.rabefaritra@gmail.com>
2024-05-26 18:22:26 +0200

    28-transactions

--- lab/28-transactions/src/test/java/rewards/SystemTestConfig.java
+++ lab/28-transactions/src/test/java/rewards/SystemTestConfig.java
@@ -5,7 +5,9 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import config.RewardsConfig;
 
@@ -32,5 +34,8 @@ public class SystemTestConfig {
 	//	TODO-02: Define a bean named 'transactionManager' that configures a
 	//           DataSourceTransactionManager.
 	//           How does it know which dataSource to manage?
-	
+	@Bean
+	public DataSourceTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(dataSource());
+	}
 }

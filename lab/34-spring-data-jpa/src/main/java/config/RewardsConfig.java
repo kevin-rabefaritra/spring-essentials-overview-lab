54ec1231b7aab100fe0007b370b668e52d1c2355
Kevin Michel <kevin.rabefaritra@gmail.com>
2024-05-29 21:57:05 +0200

    34-spring-data-jpa

--- lab/34-spring-data-jpa/src/main/java/config/RewardsConfig.java
+++ lab/34-spring-data-jpa/src/main/java/config/RewardsConfig.java
@@ -4,8 +4,8 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import rewards.RewardNetwork;
 import rewards.internal.RewardNetworkImpl;
--- lab/34-spring-data-jpa/src/main/java/rewards/internal/restaurant/RestaurantRepository.java
+++ lab/34-spring-data-jpa/src/main/java/rewards/internal/restaurant/RestaurantRepository.java
@@ -1,5 +1,6 @@
 package rewards.internal.restaurant;
 
+import org.springframework.data.repository.Repository;
 /**
  * Loads restaurant aggregates. Called by the reward network to find and reconstitute Restaurant entities from an
  * external form such as a set of RDMS rows.
@@ -9,7 +10,7 @@ package rewards.internal.restaurant;
 // TODO-06: Alter this interface to extend a proper Spring Data interface.
 // - The method name also needs refactoring (renaming) to use Spring Data finder
 //   naming conventions so Spring Data will implement it automatically for you.
-public interface RestaurantRepository {
+public interface RestaurantRepository extends Repository<Restaurant, Long> {
 
 	/**
 	 * Load a Restaurant entity by its merchant number.
@@ -17,5 +18,5 @@ public interface RestaurantRepository {
 	 * @return the restaurant
 	 */
 	// To refactor: right click on the method name -> Refactor -> Rename
-	public Restaurant findByMerchantNumber(String merchantNumber);
+	public Restaurant findFirstByNumber(String merchantNumber);
 }
